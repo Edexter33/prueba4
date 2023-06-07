854699a22422e700025de306483f4af32f373641
Edexter33 <120233584+Edexter33@users.noreply.github.com>
2023-06-07 16:36:57 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once (__DIR__.'/crest.php');
+
+$result = CRest::call(
+	'profile',
+	[
+	]
+);
+
+echo '<pre>';
+print_r($result);
+echo '</pre>';
---
+++ settings.php
@@ -0,0 +1,11 @@
+<?php
+define('C_REST_CLIENT_ID','app.6480c44e371e60.40156064');//Application ID
+define('C_REST_CLIENT_SECRET','pXpXcUXgmfyX0Q1gf5UgD45Fc6qQL0Gajv9WHI6i5Y1NMCTy7p');//Application key
+// or
+// define('C_REST_WEB_HOOK_URL','https://rest-course.bitrix24.com/rest/1/bv61hmo3otsvvf23/');//url on creat Webhook
+
+//define('C_REST_CURRENT_ENCODING','windows-1251');
+//define('C_REST_IGNORE_SSL',true);//turn off validate ssl by curl
+//define('C_REST_LOG_TYPE_DUMP',true); //logs save var_export for viewing convenience
+//define('C_REST_BLOCK_LOG',true);//turn off default logs
+//define('C_REST_LOGS_DIR', __DIR__ .'/logs/'); //directory path to save the log
